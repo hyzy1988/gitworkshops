de5f32b27ad9f3986ba40636ac76d3b4385743f9
Pawel <hyzy1988@o2.pl>
2019-12-07 11:36:13 +0100

    Pierwszy commit do projektu

---
+++ src/SecondClass.java
@@ -0,0 +1,11 @@
+/**
+ * ... comment class ...
+ *
+ * @author Paweł
+ * @since 2019-12-07
+ */
+final class SecondClass {
+    public static void main(String[] args) {
+        System.out.println("Second class on git");
+    }
+}
